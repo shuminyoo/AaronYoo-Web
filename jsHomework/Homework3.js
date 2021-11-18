1d282d7a460dbdfcdc86284567649297bfbe6c1c
Shumin Yoo <shuminyoo@Shumins-MacBook.local>
2021-11-18 11:30:31 +0900

    Third homework made

---
+++ jsHomework/Homework3.js
@@ -0,0 +1,24 @@
+//Homework
+
+let bigNum = Math.floor(Math.random() * 1000 );
+
+let registerEarly = true;
+
+let RunnerAge = 20;
+
+if (RunnerAge > 18 && registerEarly === true) {
+    bigNum += 1000;
+} else {
+    bigNum = bigNum
+}
+
+if (RunnerAge > 18 && registerEarly === true) {
+    console.log("Your race time is 9:30 am. Your race number is " + bigNum + ".")
+} else if (RunnerAge > 18 && registerEarly === false) { 
+    console.log("Your race time is 11:00am. Your race number is " + bigNum + ".")
+} else if (RunnerAge < 18) {
+    console.log("Your race time is 11:00am. Your race number is " + bigNum + ".")
+} else {
+    console.log("Please seek more guidance at the registration desk. :)")
+}
+
